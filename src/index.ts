7a47d0b053e607e1d8373771e3afb2e4ddd34728
seiry <4397354+seiry@users.noreply.github.com>
2023-07-30 22:20:50 +0800

    fix: rm do tenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
-  require('dotenv').config();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   // eslint-disable-next-line global-require
+//   require('dotenv').config();
+// }
 
 export * from './edge';
