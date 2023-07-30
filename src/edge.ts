454e7b7723a2220da009e4c97abd5dcd55d540f7
seiry <4397354+seiry@users.noreply.github.com>
2023-07-30 13:47:40 +0800

    feat: update middleware

--- src/edge.ts
+++ src/edge.ts
@@ -1,13 +1,16 @@
+/* eslint-disable no-return-await */
 // https://developers.cloudflare.com/pages/platform/functions/plugins/cloudflare-access/差点。。
 
 import { createRemoteJWKSet, jwtVerify } from 'jose';
-import type { NextRequest } from 'next/server';
+import { NextMiddlewareResult } from 'next/dist/server/web/types';
+import type { NextFetchEvent, NextMiddleware, NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 const teamDomain = process.env.TEAM_DOMAIN;
 const CERTS_URL = `${teamDomain}/cdn-cgi/access/certs`;
 
 const JWKS = createRemoteJWKSet(new URL(CERTS_URL));
+const defaultJwtName = 'CF_Authorization';
 
 const errors = {
   noToken: 'no token',
@@ -15,9 +18,12 @@ const errors = {
 };
 
 // middleware in next doesn't need to return
-// eslint-disable-next-line consistent-return
-export async function middleware(request: NextRequest) {
-  let token = request.cookies.get('CF_Authorization');
+export async function handleMiddleware(
+  request: NextRequest,
+  options?: NextJWTAuthMiddlewareOptions | undefined,
+  onSuccess?: (token: any) => Promise<NextMiddlewareResult>
+) {
+  let token = request.cookies?.get?.(options?.tokenName ?? defaultJwtName);
 
   if (process.env.NODE_ENV === 'development') {
     // NextResponse.next();
@@ -38,6 +44,7 @@ export async function middleware(request: NextRequest) {
 
   try {
     await jwtVerify(token.value, JWKS);
+    return await onSuccess?.(token);
   } catch (error) {
     return new NextResponse(JSON.stringify({ error: errors.invalidToken }), {
       status: 403,
@@ -47,3 +54,51 @@ export async function middleware(request: NextRequest) {
     });
   }
 }
+
+// https://github.com/nextauthjs/next-auth/blob/c0f9af4c567a905c9d55b732cc0610d44fbae5a6/packages/next-auth/src/next/middleware.ts#L189C1-L210C24
+
+export interface NextJWTAuthMiddlewareOptions {
+  tokenName?: string;
+}
+
+export interface NextRequestWithJWTAuth extends NextRequest {}
+
+export type NextMiddlewareWithJWTAuth = (
+  request: NextRequestWithJWTAuth,
+  event: NextFetchEvent
+) => NextMiddlewareResult | Promise<NextMiddlewareResult>;
+
+export type WithJWTAuthArgs =
+  | [NextRequestWithJWTAuth]
+  | [NextRequestWithJWTAuth, NextFetchEvent]
+  | [NextRequestWithJWTAuth, NextJWTAuthMiddlewareOptions]
+  | [NextMiddlewareWithJWTAuth]
+  | [NextMiddlewareWithJWTAuth, NextJWTAuthMiddlewareOptions]
+  | [NextJWTAuthMiddlewareOptions]
+  | [];
+
+export function withJWTAuth(...args: WithJWTAuthArgs) {
+  if (!args.length || args[0] instanceof Request) {
+    // @ts-expect-error
+    return handleMiddleware(...args);
+  }
+
+  if (typeof args[0] === 'function') {
+    const middleware = args[0];
+    const options = args[1] as NextJWTAuthMiddlewareOptions | undefined;
+    return async (...nextArgs: Parameters<NextMiddlewareWithJWTAuth>) =>
+      await handleMiddleware(
+        nextArgs[0],
+        options,
+        async () =>
+          // nextArgs[0].nextauth = { token };
+          await middleware(...nextArgs)
+      );
+  }
+
+  const options = args[0];
+  return async (...nextArgs: Parameters<NextMiddleware>) =>
+    await handleMiddleware(nextArgs[0], options);
+}
+
+export default withJWTAuth;
